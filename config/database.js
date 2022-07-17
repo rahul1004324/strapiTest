ab36c04650326640794c1e0b9701a3978d762470
EC2 Default User <ec2-user@ip-172-31-29-202.ec2.internal>
2022-07-17 19:57:12 +0000

    Installed pg, aws-S3 upload provider and updated the config files

--- config/database.js
+++ config/database.js
@@ -1,10 +1,12 @@
-const path = require('path');
-
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "postgres",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env("DATABASE_HOST", "strapidb.cvhbodzwj75y.us-east-1.rds.amazonaws.com"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME","postgres" ),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "postgres"),
     },
     useNullAsDefault: true,
   },
