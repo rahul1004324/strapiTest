12291f24f94bf8cd4e6f969dd62b4c647dd626b4
EC2 Default User <ec2-user@ip-172-31-29-202.ec2.internal>
2022-07-21 12:05:07 +0000

    sendgrid email configuration

--- config/plugins.js
+++ config/plugins.js
@@ -18,16 +18,19 @@ module.exports = ({ env }) => ({
       },
 
       }},
-	email:{
-	config:{
-	provider: 'sendgrid',
-	providerOptions:{
-	apiKey: env('SENDGRID_API_KEY'),
-	},
-	settings:{
-	defaultFrom:'rahulyadav1493@gmail.com',
-	defaultReplyTo:'rahulyadav1493@gmail.com',
-	},
-	},
-	},
+	email: {
+    provider: env('EMAIL_PROVIDER'),
+    providerOptions: {
+      host: env('EMAIL_SMTP_HOST', 'smtp.example.com'),
+      port: env('EMAIL_SMTP_PORT', 587),
+      auth: {
+        user: env('EMAIL_SMTP_USER'),
+        pass: env('EMAIL_SMTP_PASS'),
+      },
+    },
+    settings: {
+      defaultFrom: env('EMAIL_ADDRESS_FROM'),
+      defaultReplyTo: env('EMAIL_ADDRESS_REPLY'),
+    },
+  },
   });
